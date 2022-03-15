1ba12bc6184c7167e290b3429a0ec605223abfbf
wyx <763716546@qq.com>
2022-03-15 15:59:18 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: "wyx blog",
+  description: "wyx blog",
+  base: "/wyx_blog/",
+  theme: "reco",
+  locales: {
+    "/": {
+      lang: "zh-CN",
+    },
+  },
+  themeConfig: {
+    subSidebar: "auto",
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "Github", link: "https://github.com/wyx-lsy" },
+    ],
+  },
+};
